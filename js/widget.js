f1724497312ad819cd1446b771298309d311d252
Rooz.K <roozbeh.khd@gmail.com>
2018-10-29 18:11:59 -0700

    - Widget basics done

--- js/widget.js
+++ js/widget.js
@@ -1,4 +1,7 @@
 import d3 from 'd3-selection';
+import {scaleLinear, scaleOrdinal, scalePoint} from "d3-scale";
+import {max, min} from "d3-array";
+import {area} from "d3-shape";
 
 let svg = Symbol();
 
@@ -16,25 +19,49 @@ class Widget {
                 "translate(" + margin.left + "," + margin.top + ")");
 
         //extract multivariate components
-        let nodeMultivars = Object.keys(graph.nodes[0].data);
+        //let nodeMultivars = Object.keys(graph.nodes[0].data);
         let edgeMultivars = Object.keys(graph.edges[0].data);
 
-        console.log(nodeMultivars);
+        //nodeMultivars.splice(0, 4);
+        //console.log(nodeMultivars);
+        let nodeMultivars = ['pg', 'pd', 'lat', 'lng'];
+        let ids = graph.nodes
+            .filter(n => n.classes === "bus")
+            .map(n => n.data.id);
+        let idScale =
+            scalePoint()
+                .domain(ids)
+                .range([0, width]);
 
-        for(let i in nodeMultivars) {
+        for (let i in nodeMultivars) {
             let item = nodeMultivars[i];
+            let varData = graph.nodes.filter(n => n.classes==="bus")
+                .map(n => n.data[item]);
+
+            let data = ids.map((id,i) => [id, varData[i]]);
+            let multiple = this[svg].append('svg')
+                .attr('id', "multiple-" + i)
+                .attr('x', 10)
+                .attr('y', i * 100)
+                .attr('width', width)
+                .attr('height', 100)
+                .style('border', "1px solid black");
             if (typeof graph.nodes[0].data[item] === "number") {
-                this.plotNumericalVar(graph.nodes.map(n => n.data.id),
-                    graph.nodes.map(n => n.data[item]), i);
+                let yscale = scaleLinear()
+                    .domain([min(varData), max(varData)])
+                    .range([height / 10, 0]);
+
+                let areaChart = area()
+                    .x(d => idScale(d[0]))
+                    .y(d => yscale(d[1]));
+
+                multiple.append('path')
+                    .attr('d', areaChart(data))
+                    .attr('stroke', "blue")
+                    .attr('fill', "blue");
             }
         }
-
-    }
-
-    plotNumericalVar(ids, data, order) {
-        console.log(ids, data);
     }
 }
-
 
 export {Widget};
